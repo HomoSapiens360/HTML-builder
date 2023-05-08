7a215d12efbff9b151964fc95188df50b36ca786
Vagif <vaga1al@mail.ru>
2023-05-08 21:21:00 +0500

    feat: solve third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const tool = require('node:fs/promises');
+const path = require('path');
+const fs = require('fs');
+const fullPath = path.join(__dirname);
+const getFiles = async () => {
+  try {
+    const files = await tool.readdir(fullPath + '/secret-folder');
+    for (const file of files) {
+      fs.stat(fullPath + '/secret-folder/' + file, (err, stats) => {
+        if (stats.isFile()) {
+          const fSize = stats.size;
+          const fName = file.substring(0, file.lastIndexOf('.'));
+          const fExt = file.substring(file.lastIndexOf('.') + 1);
+          console.log(`${fName}-${fExt}-${fSize} b`);
+        }
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+getFiles();
